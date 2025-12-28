e7078317dc78c1a3cb3538d517973605569eddac
Luke <40768848+luke-thom@users.noreply.github.com>
2025-12-28 11:46:14 +0000

    WIP support for repeat codes

--- src/main.zig
+++ src/main.zig
@@ -20,8 +20,9 @@ pub const microzig_options = microzig.Options{
 
 // Timeings for parsing the transmission
 const transmission_time = std.time.us_per_ms * 80;
-const preabmle1_time = 9000;
-const preabmle2_time = 4500;
+const first_pulse_time = 9000;
+const first_empty_command_time = 4500;
+const first_empty_repeat_time = 2550;
 const pulse_time = 560;
 const empty_0_time = 560;
 const empty_1_time = 1690;
@@ -74,28 +75,23 @@ fn logTick() void {
     tick_index += 1;
 }
 
-fn parseTicks() void {
-    var buffer: [8 * ticks.len]u8 = undefined;
-    var writer = std.Io.Writer.fixed(&buffer);
-    for (ticks[0..tick_index], 0..) |t, i| {
-        writer.print("({}, {}), ", .{i, @intFromEnum(t)}) catch {};
-    }
-    std.log.debug("{s}", .{writer.buffered()});
-    std.log.debug("Counted {} ticks", .{tick_index});
-}
-
 fn decode() !struct {address: u16, command: u8} {
-    const preabmle1 = ticks[1].diff(ticks[0]);
-    const preabmle2 = ticks[2].diff(ticks[1]);
-    if (
-        !isAround(preabmle1, preabmle1_time)
-        or !isAround(preabmle2, preabmle2_time)
-    ) {
+    const first_pulse = ticks[1].diff(ticks[0]);
+    const first_empty = ticks[2].diff(ticks[1]);
+
+    // std.log.debug("{} {}", .{first_pulse, first_empty});
+    if (isAround(first_empty, first_empty_repeat_time)) {
+        return error.Repeat;
+    }
+    if (!isAround(first_pulse, first_pulse_time)) {
+        return error.BadHeader;
+    }
+    if (!isAround(first_empty, first_empty_command_time)) {
         return error.BadHeader;
     }
 
-    var address_low: u8 = 0;
-    var address_high: u8 = 0; 
+    var address: u16 = 0;
+    var address_high: u16 = 0; 
     var command: u8 = 0;
     var command_inverse: u8 = 0;
     for (3..tick_index) |index| {
@@ -109,7 +105,7 @@ fn decode() !struct {address: u16, command: u8} {
             const current_bits = (index-3)/2;
             const shift: u3 = @intCast(current_bits % 8);
             switch (current_bits / 8) {
-                0 => address_low |= bit << shift,
+                0 => address|= bit << shift,
                 1 => address_high |= bit << shift,
                 2 => command |= bit << shift,
                 3 => command_inverse |= bit << shift,
@@ -121,10 +117,7 @@ fn decode() !struct {address: u16, command: u8} {
         std.log.debug("Bad command", .{});
         return error.BadData;
     }
-    const address = if (
-        address_low == ~address_high
-    ) address_low
-    else (@as(u16, address_high) << 8) | address_low;
+    if (address != ~address_high) address |= address_high << 8;
     return .{.address = address, .command = command};
 }
 
